8d3bdb0c6d066482763ebf8a41bf796062cb7335
Grace Amondi <miswa.grace@gmail.com>
2023-02-24 00:06:40 +0300

    first commit

---
+++ capmanager/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-y*%=mgerw=d_+z$@poi)oku#ayxhb@7k7a1_t6=_h75#r*x9c#'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
