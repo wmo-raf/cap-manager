419b04150add637ebfc21816d8ee45ec03e87783
Grace Amondi <miswa.grace@gmail.com>
2023-05-02 17:57:07 +0300

    add translation

--- capeditor/wagtail_hooks.py
+++ capeditor/wagtail_hooks.py
@@ -2,14 +2,17 @@
 from wagtail import hooks
 from django.utils.html import format_html
 from django.templatetags.static import static
-
+from capeditor.models import Alert
+from wagtail.contrib.modeladmin.options import (
+    ModelAdmin, 
+    modeladmin_register
+)
 
 @hooks.register("insert_editor_js")
 def insert_editor_js():
     return format_html(
-        '<script src="{}"></script>',static("/capeditor/js/hide_attributes.js"),
+        '<script src="{}"></script>',static("js/hide_attributes.js"),
     )
-
 
 @hooks.register("insert_global_admin_css")
 def insert_global_admin_css():
@@ -17,3 +20,13 @@ def insert_global_admin_css():
         '<link rel="stylesheet" type="text/css" href="{}">',
         static("css/admin.css"),
     )
+
+class CAPAdmin(ModelAdmin):
+    model = Alert
+    menu_label = 'CAP Alerts'
+    menu_icon = 'cog'
+    menu_order = 200 
+    add_to_settings_menu = False
+    exclude_from_explorer = False
+
+modeladmin_register(CAPAdmin)
